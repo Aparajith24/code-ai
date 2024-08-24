cad4f8010a2483af9eeb377300322e199cb22e5e
Aparajith24 <87831591+Aparajith24@users.noreply.github.com>
2024-08-24 09:59:08 +0530

    update

--- script.sh
+++
@@ -1,8 +0,0 @@
-git add .
-
-read -p "Enter commit message: " commit_message
-git commit -m "$commit_message"
-
-git push
-
-echo "Changes have been added, committed, and pushed to the remote repository."
